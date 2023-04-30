4a2e755206c81b301ca5df3963f5b01432dd4910
Danylo Bay <danylobay@gmail.com>
2023-04-30 17:35:23 +0200

    hooks

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,7 @@ import './Button.scss';
 import PropTypes from 'prop-types';
 export const Button = ({ onClick }) => {
   return (
-    <div className="button__container">
+    <div className="btn__container">
       <button className="button" type="button" onClick={onClick}>
         Load more
       </button>
